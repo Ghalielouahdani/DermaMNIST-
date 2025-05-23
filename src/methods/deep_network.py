8431a8e7752d43c852130168298c9916817acb67
Ghali Elouahdani <ghali.elouahdani@epfl.ch>
2025-05-23 11:50:14 +0200

    CNN done

--- src/methods/deep_network.py
+++ src/methods/deep_network.py
@@ -68,11 +68,11 @@ class CNN(nn.Module):
             n_classes (int): number of classes to predict
         """
         super().__init__()
-        ##
-        ###
-        #### WRITE YOUR CODE HERE!
-        ###
-        ##
+        self.conv1 = nn.Conv2d(input_channels, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.fc1 = nn.Linear(20 * 4 * 4, 50)
+        self.fc2 = nn.Linear(50, n_classes)
+        
 
     def forward(self, x):
         """
@@ -84,11 +84,11 @@ class CNN(nn.Module):
             preds (tensor): logits of predictions of shape (N, C)
                 Reminder: logits are value pre-softmax.
         """
-        ##
-        ###
-        #### WRITE YOUR CODE HERE!
-        ###
-        ##
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        preds = self.fc2(x)
         return preds
 
 
